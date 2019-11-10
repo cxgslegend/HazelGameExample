7f98eaef4a670655d3dd6ecf3e3cfe975fe730e7
Craig Lovell <cxgslegend@gmail.com>
2019-11-10 12:15:49 -0500

    Initial Hazel Game engine code push (should match Hazel repo outside of Sandbox3d class files).

---
+++ Sandbox/src/SandboxApp.cpp
@@ -0,0 +1,24 @@
+#include <Hazel.h>
+#include <Hazel/Core/EntryPoint.h>
+
+#include "Sandbox2D.h"
+#include "Sandbox3D.h"
+
+class Sandbox : public Hazel::Application
+{
+public:
+	Sandbox()
+	{
+		//PushLayer(new Sandbox3D());
+		PushLayer(new Sandbox2D());
+	}
+
+	~Sandbox()
+	{
+	}
+};
+
+Hazel::Application* Hazel::CreateApplication()
+{
+	return new Sandbox();
+}
